aa65f174760d48184a5f3947c98f8c17982d4cf0
asoual12 <alger12c200@gmail.com>
2026-09-16 23:27:36 +0100

    Harden admin product image saves

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js';
 import { getAuth } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-auth.js';
 import { getFirestore } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-firestore.js';
-import { getStorage } from 'https://www.gstatic.com/firebasejs/12.19.0/firebase-storage.js';
 
 export const firebaseConfig = {
   apiKey: 'AIzaSyBglK-06DCU6H6VaKilzgpSgHOWLanFiCA',
@@ -15,4 +14,3 @@ export const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const storage = getStorage(app);
